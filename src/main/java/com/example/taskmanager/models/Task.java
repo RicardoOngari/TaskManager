3caf83f069bfa3bca20a9d93603226915f39e2d5
RicardoOngari <ricardo.ongari@gmail.com>
2026-09-16 22:07:39 -0300

    TaskManager

--- src/main/java/com/example/taskmanager/models/Task.java
+++ src/main/java/com/example/taskmanager/models/Task.java
@@ -1,5 +1,6 @@
 package com.example.taskmanager.models;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +10,11 @@ public class Task {
     private int id;
 
     @NotBlank(message = "Obrigatorio")
+    @Size(message = "entre 3 a 10", min = 3, max = 10)
     private String title;
 
     @NotBlank(message = "Obrigatorio")
+    @Size(message = "entre 10 a 50", min = 3, max = 50)
     private String description;
 
     private boolean completed;
